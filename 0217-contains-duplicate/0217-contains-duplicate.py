d9cdd9be468973068bb8efa7a2d10493e94834a4
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2025-02-11 13:40:48 +0530

    Time: 10 ms (80.95%), Space: 26 MB (39.98%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,11 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        seen = set()
-        for n in nums:
-            if n in seen:
-                return True
-            else:
-                seen.add(n)
+        if (len(set(nums)) != len(nums)):
+            return True
         
         return False
