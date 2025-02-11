c651c8376a60a8a5263d702f30318d6a07437b3f
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2025-02-11 16:53:57 +0530

    Time: 12 ms (60.88%), Space: 14.2 MB (62.57%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -1,6 +1,20 @@
 class Solution(object):
     def sortedSquares(self, nums):
-        squared = [n ** 2 for n in nums]
-        squared.sort()
-        return squared
+        l, r = 0, len(nums) - 1  # Two pointers
+        result = [0] * len(nums)  # Output array (same size as input)
+        k = len(nums) - 1  # Start filling from the right
         
+        while l <= r:
+            left_sq = nums[l] ** 2
+            right_sq = nums[r] ** 2
+            
+            if left_sq > right_sq:
+                result[k] = left_sq
+                l += 1  # Move left pointer forward
+            else:
+                result[k] = right_sq
+                r -= 1  # Move right pointer backward
+                
+            k -= 1  # Move to the next position in result
+            
+        return result
