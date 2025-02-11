7ca06ad7cb535381dcaae862894225f140d0eedb
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2025-02-11 16:39:02 +0530

    Time: 118 ms (34.26%), Space: 18.9 MB (87.36%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,12 @@
 class Solution(object):
     def maxProfit(self, prices):
-        l, r = 0, 1  # Left (buy) and Right (sell) pointers
         max_profit = 0  
+        l, r = 0, 1  # Left (buy) and Right (sell) pointers
 
         while r < len(prices):  # Iterate while r is within bounds
-            if prices[r] > prices[l]:  # If sell price is greater than buy price
-                max_profit = max(max_profit, prices[r] - prices[l])
+            currentProfit = prices[r] - prices[l] #our current Profit
+            if prices[l] < prices[r]:  # If buy price is less than sell price
+                max_profit = max(max_profit, currentProfit)
             else:
                 l = r  # Move buy pointer to r (new minimum price)
             r += 1  # Move sell pointer forward
