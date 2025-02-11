76a60acc8c634016a2f46bf62a99d609d249edfa
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2025-02-11 13:51:22 +0530

    Time: 23 ms (25.7%), Space: 13.5 MB (13.24%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,7 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        expected_sum = n * (n + 1) // 2
-        actual_sum = sum(nums)
-        return expected_sum - actual_sum
+        nums.sort()
+        for i,v in enumerate(nums):
+            if (i != v):
+                return v - 1
+            if v == len(nums) - 1:
+                return v + 1
