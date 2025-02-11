bd27f7461f2ec547c04cfe994d50bd2779491362
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2025-02-11 13:31:13 +0530

    Time: 12 ms (61.69%), Space: 25.9 MB (39.98%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen = set()
+        for n in nums:
+            if n in seen:
+                return True
+            else:
+                seen.add(n)
+        
+        return False
