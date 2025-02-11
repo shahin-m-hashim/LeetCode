f4b859d6ace2d70e6444b329019333fc9ffd2720
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2025-02-11 19:56:25 +0530

    Time: 685 ms (74.01%), Space: 18.3 MB (79.29%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,25 +3,28 @@ class Solution(object):
         nums.sort()
         triplets = []
         
-        for i, num in enumerate(nums):  # Avoids out-of-bounds access
+        for i, num in enumerate(nums[:-2]):  # Avoids out-of-bounds access
             if i > 0 and num == nums[i - 1]:  # Skip duplicates
                 continue
             
             left, right = i + 1, len(nums) - 1
 
             while left < right:
-                total = num + nums[left] + nums[right]
+                total = num + nums[left] + nums[right]  # Use `num` instead of `nums[i]`
 
-                if total > 0:
-                    right -= 1
-                elif total < 0:
-                    left += 1                
-                else:
+                if total == 0:
                     triplets.append([num, nums[left], nums[right]])
                     left += 1
+                    right -= 1
 
-                    while (left < right) and (nums[left] == nums[left - 1]):  # Skip duplicates
+                    while left < right and nums[left] == nums[left - 1]:  # Skip duplicates
                         left += 1
+                    while left < right and nums[right] == nums[right + 1]:  # Skip duplicates
+                        right -= 1
 
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
 
         return triplets
