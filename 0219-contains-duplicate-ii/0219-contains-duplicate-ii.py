89c91db6724d237b597871d89f3b919155756715
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2025-02-11 21:09:02 +0530

    Time: 50 ms (14.84%), Space: 18.6 MB (93.3%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,11 +1,10 @@
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
-        index_map = {}  # Store last index of each number
-        
-        for i, num in enumerate(nums):
-            if num in index_map and (i - index_map[num]) <= k:
-                return True  # Found a duplicate within k distance
-            
-            index_map[num] = i  # Update the last seen index
-        
+        seen = set()
+        for i,num in enumerate(nums):
+            if num in seen:
+                return True
+            seen.add(num)
+            if len(seen) > k:
+                seen.remove(nums[i - k])
         return False
