cbd6db14368fd4d67e46e82b478932fd368d72a2
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2025-02-12 13:41:27 +0530

    Time: 5 ms (86.59%), Space: 13.8 MB (21.23%) - LeetHub

--- 0800-letter-case-permutation/0800-letter-case-permutation.py
+++ 0800-letter-case-permutation/0800-letter-case-permutation.py
@@ -1,22 +1,14 @@
 class Solution(object):
     def letterCasePermutation(self, s):
-        result = []
+        res = []
 
-        # Helper function for backtracking
-        def backtrack(substring, index):
-            # Base case: If we reached the end, add to result
-            if index == len(s):
-                result.append(substring)
-                return
-            
-            # If the current character is a digit, add it as is
-            if s[index].isdigit():
-                backtrack(substring + s[index], index + 1)
+        def backtrack(sub = "", i = 0):
+            if len(sub) == len(s):
+                res.append(sub)
             else:
-                # Explore both lowercase and uppercase transformations
-                backtrack(substring + s[index].lower(), index + 1)
-                backtrack(substring + s[index].upper(), index + 1)
+                if s[i].isalpha():
+                    backtrack(sub + s[i].swapcase(), i + 1)
+                backtrack(sub + s[i], i + 1)
 
-        # Start backtracking from the first character
-        backtrack("", 0)
-        return result
+        backtrack()
+        return res
