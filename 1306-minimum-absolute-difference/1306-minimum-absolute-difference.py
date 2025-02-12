56e80d1f1d7f1842e0e33c0b681f97a0ab4bf3c4
Shahin M Hashim <98640366+shahin-m-hashim@users.noreply.github.com>
2025-02-12 11:10:48 +0530

    Time: 61 ms (97.14%), Space: 21.6 MB (82.29%) - LeetHub

--- 1306-minimum-absolute-difference/1306-minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.py
@@ -2,14 +2,17 @@ class Solution(object):
     def minimumAbsDifference(self, arr):
         arr.sort()
 
+        pairs = []
         min_diff = float('inf')
         for i in range(len(arr) - 1):
             diff = arr[i + 1] - arr[i]
-            min_diff = min(min_diff, diff)
-        
-        pairs = []
-        for i in range(len(arr) - 1):
-            if(arr[i + 1] - arr[i] == min_diff):
+            # If a new minimum is found, reset the result list
+            if diff < min_diff:
+                min_diff = diff
+                pairs = [[arr[i], arr[i + 1]]]
+            
+            # If the same minimum is found, append the pair
+            elif diff == min_diff:
                 pairs.append([arr[i], arr[i + 1]])
         
         return pairs
